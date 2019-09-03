80ddd63fa08099405d338c48f7afd3189034bc5b
Satel <ss251642@ncr.com>
2019-09-03 15:43:23 +0300

    Updates

---
+++ precache-manifest.28f96b08f01308d08a7596807c3b10cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6286e722c0396eccee4ef7715de02302",
+    "url": "/Robofriend/index.html"
+  },
+  {
+    "revision": "90bad808de3cc7c07542",
+    "url": "/Robofriend/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "f4615e482efd26bb5197",
+    "url": "/Robofriend/static/css/main.b9164bb9.chunk.css"
+  },
+  {
+    "revision": "90bad808de3cc7c07542",
+    "url": "/Robofriend/static/js/2.9cf59a5f.chunk.js"
+  },
+  {
+    "revision": "f4615e482efd26bb5197",
+    "url": "/Robofriend/static/js/main.6e782eac.chunk.js"
+  },
+  {
+    "revision": "a4f1039e854fb6a29b6e",
+    "url": "/Robofriend/static/js/runtime~main.f45f9dba.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
